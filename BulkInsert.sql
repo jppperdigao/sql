4ba88e12a81324714c3124623fbfb5083d65b6ef
João <joao.p.perdigao@gmail.com>
2016-10-05 12:00:07 +0200

    Update BulkInsert.sql

--- BulkInsert.sql
+++ BulkInsert.sql
@@ -6,7 +6,7 @@
 --
 --	Sintax to do a bulk insert
 --
---	Jo„o Perdig„o
+--	Jo√£o Perdig√£o
 --
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -32,13 +32,13 @@ country varchar(25) null
 BULK INSERT saturn.dbo.example
 FROM 'C:\Joao\example.csv' --source
 WITH (
- FIRSTROW = 1, --2 with header or 1 without header
- CODEPAGE = '1252',-- ANSI 1252 Windows West European Latin 1
- DATAFILETYPE = 'char',--character format (default)
- FIELDTERMINATOR = '|', --separator
- ROWTERMINATOR = '\n', --new line
- ERRORFILE =  'C:\Joao\example.csv_error.txt', --fill in the name of the error file
- TABLOCK  --speeds up the bulk insert as it locks the table as a whole
+       FIRSTROW = 1, --2 with header or 1 without header
+       CODEPAGE = '1252',-- ANSI 1252 Windows West European Latin 1
+       DATAFILETYPE = 'char',--character format (default)
+       FIELDTERMINATOR = '|', --separator
+       ROWTERMINATOR = '\n', --new line
+       ERRORFILE =  'C:\Joao\example.csv_error.txt', --fill in the name of the error file
+       TABLOCK  --speeds up the bulk insert as it locks the table as a whole
 );
 
 SELECT * FROM saturn.dbo.example; --6 rows
